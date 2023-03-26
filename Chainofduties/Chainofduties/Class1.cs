4a016f820b2eff22285d3d91acc4e7d934a3698a
bogdanborzianytsia <128235605+bogdanborzianytsia@users.noreply.github.com>
2023-03-26 16:50:44 +0300

    Add files via upload

---
+++ Chainofduties/Chainofduties/Class1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Chainofduties;
+
+namespace Chainofduties
+{
+
+    public abstract class Handler: Form1
+    {
+        protected Handler successor;
+        public void SetSuccessor(Handler successor)
+        {
+            this.successor = successor;
+        }
+        public abstract void HandleRequest(string quest, Form1 n);
+    }
+
+    public class ConcreteHandler1 : Handler
+    {
+        public override void HandleRequest(string quest, Form1 n)
+        {
+            
+            if (quest == "Hi")
+            {
+                n.richTextBox2.Text+= "Hi, how can i help you?\n";
+            }
+            else
+            {
+                successor.HandleRequest(quest, n);
+            }
+        }
+    }
+
+    public class ConcreteHandler2 : Handler
+    {
+        public override void HandleRequest(string quest, Form1 n)
+        {
+            if (quest == "Can you help me?")
+            {
+                n.richTextBox2.Text += "Yes i can help \n";
+            }
+            else
+            {
+                successor.HandleRequest(quest, n);
+            }
+        }
+    }
+
+    public class ConcreteHandler3 : Handler
+    {
+        public override void HandleRequest(string quest, Form1 n)
+        {
+            if (quest == "Can you plus numbers?")
+            {
+                n.richTextBox2.Text += "Sure i can do it\n";
+               
+            }
+            else
+            {
+                successor.HandleRequest(quest, n);
+            }
+        }
+    }
+    public class ConcreteHandler4 : Handler
+    {
+        public override void HandleRequest(string quest, Form1 n)
+        {
+            if (quest == "how much will 8 add to 13?")
+            {
+                n.richTextBox2.Text += "it will be 21\n";
+
+            }
+            else
+            {
+                n.richTextBox2.Text += "Eror";
+            }
+        }
+    }
+}
---
+++ Chainofduties/Chainofduties/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Chainofduties
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+         
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Handler h1 = new ConcreteHandler1();
+            Handler h2 = new ConcreteHandler2();
+            Handler h3 = new ConcreteHandler3();
+            Handler h4 = new ConcreteHandler4();
+            h1.SetSuccessor(h2);
+            h2.SetSuccessor(h3);
+            h3.SetSuccessor(h4);
+            h1.HandleRequest(comboBox1.Text, this);
+        }
+
+        private void richTextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
